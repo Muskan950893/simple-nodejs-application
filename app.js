fd4df8322bd0dfbf7a1f2be4052a3dee665dfe39
amitku13 <amitkumarbhontele@gmail.com>
2025-02-04 21:24:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,17 +9,3 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-root@ip-172-31-95-180:~# cat package.json
-{
-  "name": "node-app",
-  "version": "1.0.0",
-  "description": "A sample Node.js app for Jenkins CI/CD",
-  "main": "app.js",
-  "scripts": {
-    "start": "node app.js"
-  },
-  "dependencies": {
-    "express": "^4.17.1"
-  }
-}
